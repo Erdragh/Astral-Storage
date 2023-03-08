026be8eff718606d83e9aecb74cb640f37e55bc1
Jan Bayer <enderjanyt@gmail.com>
2023-03-08 23:21:45 +0100

    Initial Commit

---
+++ src/main/java/com/github/erdragh/astralstorage/AstralStorage.java
@@ -0,0 +1,13 @@
+package com.github.erdragh.astralstorage;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AstralStorage implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/erdragh/astralstorage/client/AstralStorageClient.java
@@ -0,0 +1,13 @@
+package com.github.erdragh.astralstorage.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AstralStorageClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
